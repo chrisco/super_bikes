9ff17a9bbcb1320dc614fc4d75f35951be7ae2e5
Christopher Comella <chris@Christophers-MacBook-Pro.local>
2015-09-04 14:10:34 +0200

    passing over failing bikes test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,15 @@
 require 'docking_station.rb'
-  
-describe DockingStation do 
+
+describe DockingStation do
+
+  let(:station) { DockingStation.new }
+
+  it 'creates a new instance of DockingStation' do
+    expect(station).to be_kind_of DockingStation
+  end
+
+  it 'creates an array to store bikes' do
+    expect(station.bikes).to be_kind_of Array
+  end
 
 end
